1af2e9309faeeaebf5076384f6effa72b803f342
Zaid <191896805+zaidbyte@users.noreply.github.com>
2026-03-02 17:56:22 -0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,11 @@
 // lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
+
 const supabaseUrl = 'https://ioqnwoqqnvgnytpaskkd.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvcW53b3FxbnZnbnl0cGFza2tkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0ODc3NzcsImV4cCI6MjA4ODA2Mzc3N30.rOMqMDWa8vnba2X7Prt-dEbf_1JjrveaVsSmCDtF6JY';
 

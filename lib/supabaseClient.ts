294f850282c13f646cf637bf9bff0032fbbd0a7b
Zaid <191896805+zaidbyte@users.noreply.github.com>
2026-03-02 17:50:12 -0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 // lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://ioqnwoqqnvgnytpaskkd.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvcW53b3FxbnZnbnl0cGFza2tkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0ODc3NzcsImV4cCI6MjA4ODA2Mzc3N30.rOMqMDWa8vnba2X7Prt-dEbf_1JjrveaVsSmCDtF6JY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
